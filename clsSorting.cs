fbb9bc01fbf30920eb66c5f1e2eee5b9173fd6a7
manali <manali.ajudiya@yahoo.com>
2022-10-31 07:44:48 -0700

    Sorting Algorithms
    Binary Tree and Array based Sorting
    
    C#

---
+++ clsSorting.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppCore
+{
+    class clsSorting
+    {
+        //Inplace Sorting
+        //Time Compexity - O(n^2). [n*n]
+        //Space Compexity - O(1). No additional data structures
+        public void InsertionSort(int[] input)
+        {
+            //Two Pointer
+            int i = 0;                  //Element to be sorted
+            while (i < input.Length)
+            {
+                int j = i + 1;
+                while (j < input.Length)             //Swap if ith element is greater
+                {
+                    if (input[i] > input[j])
+                    {
+                        //swap
+                        int temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            foreach (int x in input)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        //Time Complexity - O(n^2)
+        //Space Complexity O(n)
+        public void SelectionSort(int[] input)
+        {
+            //Find minimum
+            int temp = 0, _min = 0;                 //Element to be sorted
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                _min = i;                           //Set element index To compare
+                //linear scan               
+                for (int j = i + 1; j < input.Length; j++)
+                {
+
+                    if (input[j] < input[_min])
+                    {
+                        //set index
+                        _min = j;
+                    }
+
+                }
+
+                //swap elements
+                if (_min != i)
+                {
+                    temp = input[_min];
+                    input[_min] = input[i];
+                    input[i] = temp;
+                }
+
+            }
+
+            foreach (int x in input)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        //Generalized insertion sort - for sublist Gap
+        //Instead of Comparing next element, compare nth(gap)
+        //Use - to sort entries which further away first. https://www.programiz.com/dsa/shell-sort 
+        public void ShellSort(int[] input)
+        {
+            int _len = input.Length;
+            //Sublists - Sequece n/3,n/6,n/12...1          
+            for (int gap = _len; gap > 0; gap /= 3)
+            {
+                //Insertion Sort - compare 2 elements
+                for (int i = 0; i < _len; i++)
+                {
+                    for (int j = i + gap-1; j < _len; j++)
+                    {
+                        //Compare i and jth elements Distance = gap
+                        if (input[j] < input[i])
+                        {
+                            int temp = input[i];
+                            input[i] = input[j];
+                            input[j] = temp;
+                        }
+                    }
+                }
+            }         
+        }
+
+        //Divide and Conquer
+        //Divide - n/2,n/4,n/8....1
+        //Sort. Combine.
+        //Used with Linked List
+        public void MergeSort(int[] input)
+        {
+            int[] helper = new int[input.Length];
+            divide(0, input.Length-1, input, helper);
+
+            foreach (int x in input)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        private void divide(int l,int r, int[] nums,int[] helper)
+        {
+            Console.WriteLine("l:" + l + ",r:" + r);
+            if (l < r)                                              //Binary - iterates through all elements only once
+            {
+                int middle = l + (r - l) / 2;                           
+                Console.WriteLine("middle:" + nums[middle]);
+                divide(l, middle, nums, helper);
+                divide(middle+1, r, nums, helper);
+                merge(l, r, middle, nums, helper);
+            }
+        }
+
+        private void merge(int l,int middle, int r, int[] nums, int[] helper)
+        {
+            //Add elements into helper
+            for(int i = l; i < r; i++)
+            {
+                helper[i] = nums[i];
+            }
+
+            //Sort helper
+            int hL = l;
+            int hR = middle + 1;
+            int current = l;
+            //Pick smaller between left and right of helper
+            while(hL <= middle && hR <= r)
+            {
+                if(helper[hL] < helper[hR])
+                {
+                    nums[current] = helper[hL];
+                    hL++;
+                }
+                else
+                {
+                    nums[current] = helper[hR];
+                    hR++;
+                }
+                current++;
+            }
+            //Add remaining
+            int remaining = middle - hL;
+            for(int i = 0; i <= remaining; i++)
+            {
+                nums[current + i] = helper[hL + i];
+            }
+        }
+
+        //Divide and Conquer 
+        //Pick  a pivot , Smaller left + lareger right (Not sorted)
+        //Compare left elments - Sort, Sort right
+        //Complexity - log n.
+        public void QuickSort(int[] input)
+        {
+            //Choose a pivote
+            int middle = input.Length / 2;
+            int pivot = input[middle];
+
+            if (input.Length == 1)
+            {
+                if (input[0] > input[1])
+                {
+                    int temp = input[0];
+                    input[0] = input[1];
+                    input[1] = temp;
+                }
+                return;
+            }
+
+            int[] firstHalf = new int[middle];
+            //input.CopyTo(firstHalf, middle-1);
+            int[] secondHalf = new int[middle];
+            //input.CopyTo(secondHalf, input.Length);
+            Array.Copy(input, middle-1, secondHalf,secondHalf.GetUpperBound(0), input.Length - middle);
+            while (firstHalf.Length > 1)
+            {
+                QuickSort(firstHalf);
+                //QuickSort(secondHalf);
+            }
+            while (secondHalf.Length > 1)
+            {
+                //QuickSort(firstHalf);
+                QuickSort(secondHalf);
+            }
+
+            return;
+        }
+
+        //Greedy Approach https://www.geeksforgeeks.org/bubble-sort/
+        //Repeatedly swap next elements
+        //Not for large datasets
+        public void BubbleSort(int[] input)
+        {            
+            for(int x = 0;x < input.Length-1; x++)
+            {
+                for(int i= 0; i < input.Length - 1;  i++)
+                {
+                    if (input[i] > input[i + 1])
+                    {
+                        //Swap
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                    }
+                    //Console.Write(":" + input[i] + " " + input[i + 1]);
+                }
+            }            
+        }
+
+        //Binary Heap, in-place sorting https://www.youtube.com/watch?v=2DmK_H7IdTo
+        //Find minimum, place it first        
+        private void HeapSort(int[] input)
+        {
+            //Build a max heap
+        }
+    }
+
+   
+}
---
+++ clsTree.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppCore
+{
+    class clsTree
+    {
+        public void QMethod()
+        {
+            var x = new Queue<string>();
+            x.Enqueue("1");
+            x.Enqueue("2");
+            x.Enqueue("3");
+            string y = x.Dequeue();
+            Console.WriteLine(y);
+
+
+            Console.WriteLine("Count:" + x.Count);
+            /*while(x.Count > 0)
+            {
+                Console.WriteLine("El:" + x.)
+            }*/
+        }
+
+        public void TMethod()
+        {
+            int[] arr = new int[] { 2, 8, 5, 3, 9, 1 };
+            TreeNode root = CreateBinaryTree(arr);
+            Console.WriteLine("DFS");
+            DFS(root);
+            Console.WriteLine("BFS");
+            BFS(root);
+        }
+
+        private void DFS(TreeNode root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.val);
+                DFS(root.left);
+                DFS(root.right);
+            }
+            else
+            {
+                Console.WriteLine("null");
+            }
+        }
+
+        private void BFS(TreeNode root)
+        {
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                var node = q.Dequeue();
+                Console.WriteLine(node.val);
+
+
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+
+                if (node.left != null)
+                {
+                    q.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    q.Enqueue(node.right);
+                }
+            }
+        }
+
+        // Definition for a binary tree node.
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool CheckSymmetric(TreeNode root)
+        {
+            Queue q = new Queue();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                TreeNode x = (TreeNode)q.Dequeue();
+                Console.WriteLine(x.val);
+            }
+            return false;
+        }
+
+        //Create BST from Sorted Array
+        private static int[] treeEl;
+        public TreeNode CreateBinaryTree(int[] arr)
+        {
+            TreeNode root = new TreeNode();
+            treeEl = arr;
+            return helper(0, arr.Length - 1);
+        }
+
+        private TreeNode helper(int left, int right)
+        {
+            //Input : start , End position
+            if (left > right) { return null; }
+
+            //Select root index
+            int r = (left + right) / 2;
+
+            //Traversal
+            TreeNode root = new TreeNode(treeEl[r]);
+            root.left = helper(left, r - 1);
+            root.right = helper(r + 1, right);
+
+            return root;
+        }
+
+        //Use DFS.
+        //1. right 2.left - Add only 1st of the level
+        public IList<int> RightSideView(TreeNode root)
+        {
+            DFS_R(root,0);
+            return nodes;
+        }
+
+
+        private List<int> nodes = new List<int>();
+        private void DFS_R(TreeNode root, int l)
+        {
+            if (root != null)
+            {
+                //Add only first elements that comes(right)
+                if (l == nodes.Count)
+                {
+                    nodes.Add(root.val);
+                }
+                //{ Console.WriteLine("val:" + root.val + ",l:" + l);}
+                if (root.right != null) { DFS_R(root.right, l + 1); }
+                if (root.left != null)
+                {
+                    //Console.WriteLine("left:" + root.left.val);
+                    DFS_R(root.left, l + 1);
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
